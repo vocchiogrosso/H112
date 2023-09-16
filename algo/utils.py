daee4bca87307b045ea6c48a8b05235340130214
Flaze <18499438+FlazeNaive@users.noreply.github.com>
2023-09-17 00:34:56 +0200

    add utils.py(actually a test)

--- algo/utils.py
+++ algo/utils.py
@@ -1,6 +1,8 @@
-from classes.RouteClass import InboundRoute, OutboundRoute
-from classes.TruckClass import Truck
-from classes.LocationClass import Location, PlantLoc, ProviderLoc, ClientLoc
+import sys
+sys.path.append("algo/classes")
+from RouteClass import InboundRoute, OutboundRoute
+from TruckClass import Truck
+from LocationClass import Location, PlantLoc, ProviderLoc, ClientLoc
 
 import json
 
@@ -24,3 +26,33 @@ def recommend_inbound(providers, order):
     
     # return the closest provider
     return dist[0][0]
+
+PATH = "./data/"
+tmp = json.load(open(PATH + "infoPlants.json", "r"))['Outbound ARG']
+plants = []
+for plant in tmp:
+    print(plant)
+    plants.append(PlantLoc(plant, tmp[plant]['Plant Name'], tmp[plant]['Latitude'], tmp[plant]['Longitude']))
+print("")
+
+tmp = json.load(open(PATH + "infoClients.json", "r"))['Outbound ARG']
+clients = []
+for client in tmp:
+    print(client)
+    clients.append(ClientLoc(client, tmp[client]['Latitude'], tmp[client]['Longitude'], tmp[client]['City']))
+print("")
+
+tmp = json.load(open(PATH + "infoProviders.json", "r"))['Inbound ARG']
+providers = []
+for provider in tmp:
+    print(provider)
+    providers.append(ProviderLoc(provider, tmp[provider]['Origin'], tmp[provider]['Latitude'], tmp[provider]['Longitude'], tmp[provider]['Material Code'], tmp[provider]['Material']))
+print("")
+
+v_order1 = Order("SLEEP, FOOD, AND CAT!!!", 10, clients[0], plants[0])    
+rec = recommend_inbound(providers, v_order1)
+print(rec.provider_id, rec.origin, rec.material_name, rec.material_code)
+print("Plant: ", v_order1.plant.plant_id, v_order1.plant.plant_name, v_order1.plant.latitude, v_order1.plant.longitude)
+print("Client: ", v_order1.client.client_id, v_order1.client.city, v_order1.client.latitude, v_order1.client.longitude)
+print("Provider: ", rec.provider_id, rec.origin, rec.latitude, rec.longitude)
+print(rec.haversine(v_order1.client))

411443117f5d087cc75be8b4fc0c40730fd5dceb
Vincent O <16623151+vocchiogrosso@users.noreply.github.com>
2023-09-16 18:20:31 +0200

    09.16.2023_18:19
    + Haversine Example

--- api/src/routes/v1/contractor/contractor.controller.ts
+++ api/src/routes/v1/contractor/contractor.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { ContractorService } from './contractor.service';
 
-@Controller('example')
+@Controller('contractor')
 export class ContractorController {
   constructor(private readonly contractorService: ContractorService) {}
 
-  @Get()
+  @Get('h')
   getHello(): string {
     return this.contractorService.getHello();
   }
+
+  @Post('a')
+  getPost(): 
 }
